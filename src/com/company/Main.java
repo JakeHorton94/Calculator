b2eb4331875366d012bde2728a52380c2b3b01ed
Jake Horton <jake.horton@softwire.com>
2019-10-27 16:09:57 +0000

    now able to add more than 2 numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,27 +3,25 @@ package com.company;
 import java.awt.*;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the calculator!");
 
-
-//      Enter numbers
-        System.out.println("Enter first number");
-        Scanner scanner = new Scanner(System.in);
-        int FirstNumber = scanner.nextInt();
-
-        System.out.println("Enter Second number");
-        int SecondNumber = scanner.nextInt();
-
 //      Choose Operation
         Calculation calculationInstance = chooseOperation();
 
-//      Perform the calculation
-        int result = calculationInstance.calculate(FirstNumber, SecondNumber);
+//      Choose Numbers
+        int[] numbers = chooseNumbers();
+
+//        Calculate result
+        int result = calculate(calculationInstance, numbers);
+
+//       Print result
         System.out.println("Result: " + result);
+
     }
 
     //
@@ -44,44 +42,32 @@ public class Main {
         }
     }
 
+    private static int[] chooseNumbers() {
+//        ask how many numbers
+        System.out.println("How many numbers will you be using?");
+        Scanner scanner = new Scanner(System.in);
+        int numberCount = scanner.nextInt();
+
+//        Read numbers
+        int[] numbers = new int[numberCount];
+        for (int i = 1; i <= numberCount; i++) {
+            System.out.println("Enter number " + i);
+            numbers[ i - 1] = scanner.nextInt();
+        }
+        return numbers;
+    }
+
+//    Calculate
+    private static int calculate(Calculation calculation, int[] numbers) {
+        int result = numbers[0];
+        for (int i = 1; i <= numbers.length; i++) {
+            result = calculation.calculate(result, numbers[i-1]);
+        }
+        return result;
+    }
+
+
 }
-
-//        print calculation
-
-
-
-//        Generate two random numbers
-//        Random random = new Random();
-//        int FirstNumber = random.nextInt(100);
-//        int SecondNumber = random.nextInt(100);
-//        System.out.println("First number: " + FirstNumber);
-//        System.out.println("Second number: " + SecondNumber);
-
-//        if (operation.equals("+")){
-//            result = FirstNumber + SecondNumber;
-//        }
-//        if (operation.equals("-")){
-//            result = FirstNumber - SecondNumber;
-//        }
-//        if (operation.equals("*")){
-//            result = FirstNumber * SecondNumber;
-//        }
-
-        //        switch (input) {
-//            case "+":
-//                result = FirstNumber + SecondNumber;
-//                break;
-//            case "-":
-//                result = FirstNumber - SecondNumber;
-//                break;
-//            case "*":
-//                result = FirstNumber * SecondNumber;
-//                break;
-//            case "/":
-//                result = FirstNumber / SecondNumber;
-//            default:
-//                throw new IllegalArgumentException("You must enter one of (+,-,*,/)");
-//        }
 
 
 
